0d9ae2df6f43a93cd13c06464bf6027fe29ced6e
Tyler McCardle <68253755+TreeGreenWish@users.noreply.github.com>
2024-05-15 18:25:17 -0500

    Add files via upload

---
+++ cutupstory.py
@@ -0,0 +1,15 @@
+import random
+
+def cut_up_machine(*stories):
+    sentences = []
+    for story in stories:
+        sentences += story.split('. ')
+    random.shuffle(sentences)
+    return '. '.join(sentences)
+
+# Test the function
+story1 = "Once upon a time, in a land far far away, there lived a beautiful princess."
+story2 = "The prince was on a journey to find a potion that would make him immortal."
+story3 = "They met and fell in love, but the wicked witch was determined to keep them apart."
+
+print(cut_up_machine(story1, story2, story3))
